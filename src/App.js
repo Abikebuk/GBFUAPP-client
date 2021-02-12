9b397aff184d3bf6c1b686930e17923bef63ae83
Rettana <rettana.muon@gmail.com>
2021-02-12 17:44:45 +0100

    adds dotenv

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.scss";
 import "bootstrap/scss/bootstrap.scss";
 import RaidCanvas from "./components/RaidCanvas";
-
+import * as dotenv from 'dotenv';
 
 function App() {
-    console.log(process.env);
-  return (
+    dotenv.config();
+    console.log(process.env)
+    return (
     <div className="App">
         <div className='container-fluid'>
             <header className="App-header row no-gutters">
